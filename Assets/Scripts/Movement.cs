e8ebb0089ae982a64012a5d1bfeb7a11b745b3a0
Caleb Turner <turner632@morrisville.edu>
2022-09-14 12:20:38 -0400

    AddRelativeForce

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed space");
+            rb.AddRelativeForce(Vector3.up);
         }
     }
     void ProcessRotation()
